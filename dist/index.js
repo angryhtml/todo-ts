566585879c77bd35ee43ab1f61421e2a45c8fcc6
AngryHtml <123128711+angryhtml@users.noreply.github.com>
2025-07-06 22:51:57 +0200

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,41 @@
+"use strict";
+const btn = document.getElementById('btn');
+const input = document.getElementById('todoinput');
+const form = document.getElementById('formtodo');
+const list = document.getElementById('todolist');
+const todos = readTodos();
+todos.forEach(createTodo);
+function readTodos() {
+    const todosJSON = localStorage.getItem('todos');
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+function handleSubmit(e) {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    input.value = '';
+}
+function createTodo(todo) {
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLi.append(todo.text);
+    newLi.append(checkbox);
+    list.append(newLi);
+}
+form.addEventListener('submit', handleSubmit);
